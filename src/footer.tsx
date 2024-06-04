31bb93a36d50db86b798eaced5b1451eb1e1c604
Abel Semahegn <abel249733@gmail.com>
2024-06-04 11:16:01 -0500

    Test

---
+++ src/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return(
+        <>
+        <div className="text-center">
+           <h1 className=" bg-gradient-to-r from-yellow-200 via-emerald-200 to-indigo-400 inline-block text-transparent bg-clip-text">Content is available under Attribution-NonCommercial-ShareAlike 4.0 unless otherwise noted.</h1>
+        </div>
+        </>
+    )
+}
